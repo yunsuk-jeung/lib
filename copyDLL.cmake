5c8991c991820e14364ae530894ab3ff5161e8d5
ysjeung <yunsukjeung@snu.ac.kr>
2024-01-10 19:35:55 +0900

    copy dll 수정

--- copyDLL.cmake
+++ copyDLL.cmake
@@ -2,7 +2,7 @@ function(copy_tbb_dlls target EXTERNAL_LIB_DIR)
   set(TBB_BIN_DIR ${EXTERNAL_LIB_DIR}/libs_win/oneTBB-2021.11.0-rc1/bin)
 
   if(WIN32)
-      if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
       file(GLOB TBB_DLLS "${TBB_BIN_DIR}/tbb*_debug.dll")
     else()
       file(GLOB TBB_DLLS "${TBB_BIN_DIR}/tbb*.dll")
@@ -20,7 +20,7 @@ function(copy_cv34_dlls target)
   set(CV_BIN_DIR ${EXTERNAL_LIB_DIR}/libs_win/opencv-3.4.20/x64/vc17/bin)
 
   if(WIN32)
-      if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
       file(GLOB CV_DLLS "${CV_BIN_DIR}/opencv*d.dll")
       list(APPEND CV_DLLS "${CV_BIN_DIR}/opencv_ffmpeg3420_64.dll")
     else()
@@ -32,5 +32,42 @@ function(copy_cv34_dlls target)
       COMMAND ${CMAKE_COMMAND} -E copy
       ${CV_DLLS}
       $<TARGET_FILE_DIR:${target}>)
+  endif()
+endfunction()
+
+function(copy_tbb_dlls_to_dir target EXTERNAL_LIB_DIR DST_DIR)
+  set(TBB_BIN_DIR ${EXTERNAL_LIB_DIR}/libs_win/oneTBB-2021.11.0-rc1/bin)
+
+  if(WIN32)
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+      file(GLOB TBB_DLLS "${TBB_BIN_DIR}/tbb*_debug.dll")
+    else()
+      file(GLOB TBB_DLLS "${TBB_BIN_DIR}/tbb*.dll")
+      list(FILTER TBB_DLLS EXCLUDE REGEX ".*_debug\\.dll$")
+    endif()
+
+    add_custom_command(TARGET ${target} POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E copy
+      ${TBB_DLLS}
+      ${DST_DIR})
+  endif()
+endfunction()
+
+function(copy_cv34_dlls_to_dir target EXTERNAL_LIB_DIR DST_DIR)
+  set(CV_BIN_DIR ${EXTERNAL_LIB_DIR}/libs_win/opencv-3.4.20/x64/vc17/bin)
+
+  if(WIN32)
+    if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+      file(GLOB CV_DLLS "${CV_BIN_DIR}/opencv*d.dll")
+      list(APPEND CV_DLLS "${CV_BIN_DIR}/opencv_ffmpeg3420_64.dll")
+    else()
+      file(GLOB CV_DLLS "${CV_BIN_DIR}/opencv*.dll")
+      list(FILTER CV_DLLS EXCLUDE REGEX ".*d\\.dll$")
+    endif()
+
+    add_custom_command(TARGET ${target} POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E copy
+      ${CV_DLLS}
+      ${DST_DIR})
   endif()
 endfunction()
